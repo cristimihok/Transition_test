3556b4fc17aa3575fe444796f6fd345a80bfc1f3
Cristian Mihok <cristian.mihok@tss-yonder.com>
2014-05-27 19:15:42 +0300

    Second commit

--- app/js/app.js
+++ app/js/app.js
@@ -22,7 +22,9 @@ angular.module('myApp', ['ngRoute', 'ngAnimate', 'myApp.filters', 'myApp.service
 
 	.otherwise({
 		redirectTo: '/view1'
-	})
+	});
+
+	//document.getElementById('test').addEventListener('click', function(){alert('TEST')}, false);
 });
 
 angular.module('myApp')
@@ -33,6 +35,38 @@ angular.module('myApp')
 	$scope.changePage = function () {
 		$location.path('/view2')
 	}
+
+	$scope.showMenu = function () {
+		$('#st-container').addClass('st-menu-open');
+	}
+
+	setTimeout(function () {
+		$("#owl-example").owlCarousel({
+			items: 1,
+			itemsScaleUp: true,
+			slideSpeed: 1000,
+			navigation: true,
+			pagination: true,
+			responsive: true,
+			itemsDesktopSmall: [979, 1],
+			itemsTablet: [768, 1],
+			itemsMobile: [479, 1]
+		});
+
+		var owl = $("#owl-example").data('owlCarousel');
+
+		setInterval(function () {
+			owl.next();
+		}, 2000)
+	},0);
+
+})
+
+.controller('MainCtrl', function ($scope) {
+
+	$scope.toggleMenu = function () {
+		$('#st-container').toggleClass('st-menu-open');
+	}
 })
 
 .controller('View2', function($scope, $location) {
@@ -41,4 +75,21 @@ angular.module('myApp')
 	$scope.changePage = function () {
 		$location.path('/view1')
 	}
-})
+});
+
+var closeMenu = function () {
+	$('#st-container').removeClass('st-menu-open');
+}
+
+var showMenu = function () {
+	// var elem = document.getElementById('st-container');
+	// console.log(elem);
+	// elem.className = elem.className + ' st-menu-open';
+	console.log($('#st-container'));
+	$('#st-container').toggleClass('st-menu-open');
+}
+
+
+
+
+
