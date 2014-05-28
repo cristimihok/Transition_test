13d47b6eebe4eb6a8e3fe75a3aee8ff9205b0574
Cristian Mihok <cristian.mihok@tss-yonder.com>
2014-05-28 20:54:35 +0300

    Refactored css files

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 /* http://docs.angularjs.org/#!angular.service */
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['ngRoute', 'ngAnimate', 'myApp.filters', 'myApp.services'])
+angular.module('myApp', ['ngRoute', 'ngAnimate', 'myApp.filters', 'myApp.services', 'myApp.controllers'])
 .run(function ($rootScope) {
 	// $rootScope.$on('$locationChangeSuccess', function() {
 	// 	alert('CHANGE')
@@ -23,71 +23,11 @@ angular.module('myApp', ['ngRoute', 'ngAnimate', 'myApp.filters', 'myApp.service
 	.otherwise({
 		redirectTo: '/view1'
 	});
-
-	//document.getElementById('test').addEventListener('click', function(){alert('TEST')}, false);
 });
 
-angular.module('myApp')
 
-.controller('View1', function($scope, $location) {
-	$scope.cls = 'view1';
 
-	$scope.changePage = function () {
-		$location.path('/view2')
-	}
 
-	$scope.showMenu = function () {
-		$('#st-container').addClass('st-menu-open');
-	}
-
-	setTimeout(function () {
-		$("#owl-example").owlCarousel({
-			items: 1,
-			itemsScaleUp: true,
-			slideSpeed: 1000,
-			navigation: true,
-			pagination: true,
-			responsive: true,
-			itemsDesktopSmall: [979, 1],
-			itemsTablet: [768, 1],
-			itemsMobile: [479, 1]
-		});
-
-		var owl = $("#owl-example").data('owlCarousel');
-
-		setInterval(function () {
-			owl.next();
-		}, 2000)
-	},0);
-
-})
-
-.controller('MainCtrl', function ($scope) {
-
-	$scope.toggleMenu = function () {
-		$('#st-container').toggleClass('st-menu-open');
-	}
-})
-
-.controller('View2', function($scope, $location) {
-	$scope.cls = 'view2';
-
-	$scope.changePage = function () {
-		$location.path('/view1')
-	}
-});
-
-var closeMenu = function () {
-	$('#st-container').removeClass('st-menu-open');
-}
-
-var showMenu = function () {
-	// var elem = document.getElementById('st-container');
-	// console.log(elem);
-	// elem.className = elem.className + ' st-menu-open';
-	console.log($('#st-container'));
-	$('#st-container').toggleClass('st-menu-open');
-}
 
 
 
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,68 @@
 'use strict';
 /* App Controllers */
 
+angular.module('myApp.controllers', [])
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+.controller('MainCtrl', function ($scope) {
 
+	$scope.toggleMenu = function () {
+		$('#st-container').toggleClass('st-menu-open');
+	}
 
-function MyCtrl2() {
-}
-MyCtrl2.$inject = [];
+	$scope.openMenu = function () {
+		setTimeout(function(){
+			$('#st-container').addClass('st-menu-open');
+		},200)
+	}
+
+	$scope.closeMenu = function () {
+		setTimeout(function(){
+			$('#st-container').removeClass('st-menu-open');
+		},500)
+	}
+
+	$scope.menuItems = [
+		{class: 'icon icon-data', href: '#view1', name: 'View 1'},
+		{class: 'icon icon-location', href: '#view2', name: 'View 2'}
+	]
+})
+
+.controller('View1', function($scope, $location) {
+	$scope.cls = 'view1';
+
+	$scope.changePage = function () {
+		$location.path('/view2')
+	}
+
+	$scope.showMenu = function () {
+		$('#st-container').addClass('st-menu-open');
+	}
+
+	setTimeout(function () {
+		$("#owl-example").owlCarousel({
+			items: 1,
+			itemsScaleUp: true,
+			slideSpeed: 1000,
+			navigation: true,
+			pagination: true,
+			responsive: true,
+			itemsDesktopSmall: [979, 1],
+			itemsTablet: [768, 1],
+			itemsMobile: [479, 1]
+		});
+
+		var owl = $("#owl-example").data('owlCarousel');
+
+		setInterval(function () {
+			owl.next();
+		}, 2000)
+	},0);
+})
+
+.controller('View2', function($scope, $location) {
+	$scope.cls = 'view2';
+
+	$scope.changePage = function () {
+		$location.path('/view1')
+	}
+});
